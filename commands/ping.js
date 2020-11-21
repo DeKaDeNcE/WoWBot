71826e5d0eee8d005585308c295f68eeca4d447b
DeKaDeNcE <admin@dekadence.ro>
2020-11-21 03:23:09 +0200

    Initial commit

---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+// noinspection JSUnresolvedVariable
+module.exports = {
+	name: 'ping',
+	description: 'Ping!',
+	cooldown: 5,
+	execute: (message, args) => {
+		message.channel.send('Pong.')
+	}
+}
